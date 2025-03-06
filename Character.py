7a5a4c11bcb971c443f03fb060324e00c6124302
Zoivitos <46927682+Zoivitos@users.noreply.github.com>
2025-03-06 13:35:38 +0100

    Added Character Clothe and Position Class

---
+++ Character.py
@@ -0,0 +1,18 @@
+import Clothe
+class Character:
+    """
+    Represents a character with an ID, a list of clothes, and a file path.
+    """
+
+    def __init__(self, id, clothes, path_to_file):
+        """
+        Initializes a Character object.
+
+        Args:
+            id (int): The unique identifier of the character.
+            clothes (list): A list of Clothe objects associated with the character.
+            path_to_file (str): The file path related to the character.
+        """
+        self.id = id
+        self.clothes = clothes
+        self.path_to_file = path_to_file
